a10b88c66bed7f5f96edfaa2dbfb609620c8e716
Katarina Krstin <92788177+kkatarinna@users.noreply.github.com>
2023-02-03 15:22:34 +0100

    lru update

--- Projekat/Structures/LRU.go
+++ Projekat/Structures/LRU.go
@@ -2,53 +2,68 @@ package structures
 
 import (
 	"container/list"
+	"fmt"
 )
 
 type Cache struct {
-    capacity int
-    list     *list.List
-    items    map[string]list.Element
+	capacity int
+	list     *list.List
+	items    map[string]*list.Element
 }
 
 type Entry struct { //kao neka struktura za podatke, moze da se izmeni ako treba
-    key   string
-    Value []byte
+	key   string
+	Value []byte
 }
 
 func NewCache(capacity int) *Cache { //konstruktor za cache
-    return &Cache{
-        capacity: capacity,
-        list:     list.New(),
-        items:    make(map[string]list.Element),
-    }
+	return &Cache{
+		capacity: capacity,
+		list:     list.New(),
+		items:    make(map[string]*list.Element),
+	}
 }
 
-func (c Cache) Set(key string, value []byte) {
-    if elem, ok := c.items[key]; ok { //stavlja element ispred svih u ;isti ukoliko postoji vec element u njoj
-        c.list.MoveToFront(&elem)
-        elem.Value.(*Entry).Value = value
-        return
-    }
-    e := c.list.PushFront(&Entry{key, value}) //samo postavi novi element
-    c.items[key] = *e
-    if c.list.Len() > c.capacity {
-        c.RemoveOldest()
-    }
+func (c *Cache) Set(key string, value []byte) {
+	if elem, ok := c.items[key]; ok { //stavlja element ispred svih u ;isti ukoliko postoji vec element u njoj
+		c.list.MoveToFront(elem)
+
+		elem.Value.(*Entry).Value = value
+		return
+	}
+	e := c.list.PushFront(&Entry{key, value}) //samo postavi novi element
+	c.items[key] = e
+	if c.list.Len() > c.capacity {
+		c.RemoveOldest()
+	}
 }
 
-func (c Cache) Get(key string) ([]byte, bool) {
-    if e, ok := c.items[key]; ok {
-        c.list.MoveToFront(&e)
-        return e.Value.(Entry).Value, true
-    }
-    return nil, false
+func (c *Cache) Get(key string) ([]byte, bool) {
+	if e, ok := c.items[key]; ok {
+		c.list.MoveToFront(e)
+		return e.Value.(*Entry).Value, true
+	}
+	return nil, false
 }
 
-func (c Cache) RemoveOldest() {
-    e := c.list.Back()
-    if e != nil {
-        c.list.Remove(e)
-        kv := e.Value.(Entry)
-        delete(c.items, kv.key)
-    }
+func (c *Cache) RemoveOldest() {
+	e := c.list.Back()
+	if e != nil {
+		c.list.Remove(e)
+		kv := e.Value.(*Entry)
+		delete(c.items, kv.key)
+	}
+}
+
+func main() {
+	fmt.Println("LRU cache")
+	c := NewCache(3)
+	c.Set("Katarina", []byte{1, 2, 3})
+	c.Set("Milos", []byte{4, 5, 6})
+	//c.Set(2, 2)
+	fmt.Println(c.Get("Katarina"))
+	m := NewMem()
+	m.Insert("Katarina", []byte("jhikg"))
+	fmt.Println(m.Find("Katarina"))
+
 }
